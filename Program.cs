f6dd37249f10cd73ee9218e68bb24010c4779205
kauansantts <santoskauan78910@gmail.com>
2026-08-06 12:24:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Começo do projeto!");
+        }
+    }
+}
